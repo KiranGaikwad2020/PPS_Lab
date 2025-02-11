bf4cca0ff5ca756fdeeb3ee9f7810090e5f4edb2
KiranGaikwad <kiran.gaikwad@mmit.edu.in>
2025-02-11 14:20:50 +0530

    Developed arithmetical operations python file

---
+++ arithmetical.py
@@ -0,0 +1,24 @@
+#This program demonstrates the arithmetical operations
+num1=float(input("\n Enter the first number: "))
+num2=float(input("\n Enter the second number: "))
+print("\n--------------------------------------------------------")
+print("****Following is the result of Addition ****")
+result1=num1+num2
+print("\n Addition of given number {} and {} is: {}".format(num1,num2,result1))
+print("\n--------------------------------------------------------")
+print("****Following is the result of Substraction ****")
+result2=num1-num2
+print("\n Substraction of given number {} and {} is: {}".format(num1,num2,result2))
+print("\n\n--------------------------------------------------------")
+print("****Following is the result of Multiplication ****")
+result3=num1*num2
+print("\n Multiplication of given number {} and {} is: {}".format(num1,num2,result3))
+print("\n--------------------------------------------------------")
+print("****Following is the result of Division ****")
+result4=num1//num2
+print("\n Division of given number {} and {} is: {}".format(num1,num2,result4))
+print("\n--------------------------------------------------------")
+print("****Following is the result of Modulus ****")
+result5=num1%num2
+print("Modulus of given number {} and {} is: {}".format(num1,num2,result5))
+
