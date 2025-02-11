07f89b0e7e2b0a09e27a9e12820c7be758efe7bb
Kiran Gaikwad <kiranmit2015@gmail.com>
2025-02-11 12:39:04 +0530

    simple python program added

---
+++ simple.py
@@ -0,0 +1,18 @@
+#This python program demonstrates the arithmetical operation (Addition)
+
+num1=int(input("Enter the First number: "))
+num2=int(input("Enter the Second number: "))
+result=num1+num2
+print("Addition of given two numbers {} and {} is: {}".format(num1,num2,result))
+
+
+
+'''
+#Following lines show the execution of the above program
+OutPut
+comp412@comp412-desktop:~/FE_2024-25/pythonprograms$ python3 simple.py 
+Enter the First number: 34
+Enter the Second number: 56
+Addition of given two numbers 34 and 56 is: 90
+comp412@comp412-desktop:~/FE_2024-25/pythonprograms$ '''
+
