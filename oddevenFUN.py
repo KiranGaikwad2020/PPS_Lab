065bf9cdae5b0be0b4fd4dff12152ca7e4890586
KiranGaikwad <kiran.gaikwad@mmit.edu.in>
2025-03-03 10:01:08 +0530

    Even/Odd Program using function developed

---
+++ oddevenFUN.py
@@ -0,0 +1,9 @@
+#This program demosntrates the identification of even/odd number using function
+def evenodd():
+	number=int(input("\n Enter the number"))
+	if(number%2==0):
+		print("\n Entered number {} is even number".format(number))
+	else:
+		print("\n Entered number {} is odd number".format(number))
+
+evenodd()
