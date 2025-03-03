8a923e4de2766d8cbe3d2bbf645e39479800da19
KiranGaikwad <kiran.gaikwad@mmit.edu.in>
2025-03-03 15:09:55 +0530

    Module Directory added

---
+++ module/vowel.py
@@ -0,0 +1,15 @@
+#This program verify the vowel or consonants 
+def vowelfun():
+
+	ch=input("\n Enter the charcter::-----")
+#if ch in 'aeiou':
+
+	if(ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch =='U'):
+
+		print("\n Enterd charcter is vowel")
+	elif(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch =='u'):
+
+		print("\n Enterd charcter is vowel")	
+	else:
+		print("\n Not vowel")
+
