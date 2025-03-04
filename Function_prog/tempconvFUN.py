f1219732e2b173ebdf621519739548a72e88cd98
Kiran Gaikwad <kiranmit2015@gmail.com>
2025-03-04 11:00:37 +0530

    python program on evenodd and temprature conversion using functions added

--- Function_prog/tempconvFUN.py
+++ Function_prog/tempconvFUN.py
@@ -3,7 +3,6 @@ def tempconv():
 	temp_far=float(input("\n Enter the temperature in farheneit"))
 	print("\n Converting given farheneit temprature into celcius")
 	temp_celc=round((temp_far-32)*float(5/9),2)
-	print(temp_celc)
 	print("\n Given Temperature in farheneit is {}.And it's conversion into celcious is {}".format(temp_far,temp_celc,".2f"))
 	
 tempconv()
