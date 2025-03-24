3575feaf6cd7a3edce542d05521d6de250b02fef
KiranGaikwad <kiran.gaikwad@mmit.edu.in>
2025-03-24 13:38:55 +0530

    FIle Handling Programs included

---
+++ File_Handling/filetest.py
@@ -0,0 +1,6 @@
+#This program demosntrates the file handling
+filept=open("sum.txt","r")
+if(filept!=0):
+	print("\n File is opened in read mode")
+else:
+	print("\n File is not opened in read mode")
