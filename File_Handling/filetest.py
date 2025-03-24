cda891efc2ee55a7988d023b3991a9d8bf73bc95
KiranGaikwad <kiran.gaikwad@mmit.edu.in>
2025-03-24 14:44:34 +0530

    File is updated with open() and close() test

--- File_Handling/filetest.py
+++ File_Handling/filetest.py
@@ -1,6 +1,42 @@
-#This program demosntrates the file handling
-filept=open("sum.txt","r")
-if(filept!=0):
-	print("\n File is opened in read mode")
-else:
-	print("\n File is not opened in read mode")
+#This program demosntrates the file handling operations
+
+#1. Open()
+def openfiletest():
+	filept=open("/home/comp625/test.py","r")
+	if(filept!=0):
+		print("\n File is opened in read mode and original contents are \n")
+		print("***********************************************************")
+		print(filept.read())
+		print("***********************************************************")
+		filept.close()
+	else:
+		print("\n File is not opened in read mode")
+	
+	
+#2. Close()
+def closefiletest():
+	fileopt=open("/home/comp625/vote2.py","r+")
+
+
+#	fileinput=input("\n Enter the name of file alongwith file extesnion")
+#	fileopt=open("fileinput","r+")
+	if(fileopt!=0):
+		print("\n File is opened in Read & Write Mode")
+		print("\n Original Contenetns of the file are \n")
+		print("*********************************************")
+		print(fileopt.read())
+		print("*********************************************")
+		print("\n File is updating with writing single line: #This is added line")
+		fileopt.write("#This is added line")
+		fileopt.close()
+	else:
+		print("\n File is not opened for the operation")
+	fileopt=open("/home/comp625/vote2.py","r")
+	print("\n File is opened after write operation performed")
+	print("*****************************************************************")
+	print(fileopt.read())
+	print("******************************************************************")
+	fileopt.close()
+	
+#openfiletest()			
+closefiletest()
